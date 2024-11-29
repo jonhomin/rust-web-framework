032444b249f74be75bf125144f3c43a1903c3d7f
jonhomin <jon@ozway.jp>
2024-11-30 00:37:56 +0900

    Fixed admin home component

--- frontend/src/pages/admin/AdminHome.tsx
+++ frontend/src/pages/admin/AdminHome.tsx
@@ -1,14 +1,9 @@
-import { Typography, Paper } from "@mui/material";
+import { Typography } from "@mui/material";
 
 export const AdminHome = () => {
   return (
-    <Paper sx={{ p: 3 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Admin Dashboard
-      </Typography>
-      <Typography variant="body1">
-        Welcome to the admin area. This is your dashboard.
-      </Typography>
-    </Paper>
+    <Typography variant="h4" component="h1" gutterBottom>
+      hello world!
+    </Typography>
   );
 };
