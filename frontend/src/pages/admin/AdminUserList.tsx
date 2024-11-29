d1530e9d490df487413d0bdb450f8401dc18dfd1
jonhomin <jon@ozway.jp>
2024-11-30 00:47:37 +0900

    Added admin user list

---
+++ frontend/src/pages/admin/AdminUserList.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export const AdminUserList = () => {
+  return (
+    <Typography variant="h4" component="h1" gutterBottom>
+      hello world!!
+    </Typography>
+  );
+};
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import { PublicLayout } from "./layouts/PublicLayout";
 import { AdminLayout } from "./layouts/AdminLayout";
 import { Home } from "./pages/public/Home";
 import { AdminHome } from "./pages/admin/AdminHome";
+import { AdminUserList } from "./pages/admin/AdminUserList";
 
 export const Router = () => {
   return (
@@ -16,6 +17,9 @@ export const Router = () => {
         {/* Admin routes */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminHome />} />
+        </Route>
+        <Route path="/admin/users" element={<AdminLayout />}>
+          <Route index element={<AdminUserList />} />
         </Route>
       </Routes>
     </BrowserRouter>
